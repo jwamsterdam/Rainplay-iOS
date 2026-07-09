b3eac6ad1ecb11ed2616f19915dff6e574d4824d
jwamsterdam <jwhennink@gmail.com>
2026-07-09 23:01:00 +0200

    Initial Commit

---
+++ Rainplay iOS/Rainplay_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rainplay_iOSApp.swift
+//  Rainplay iOS
+//
+//  Created by jan.willem.hennink on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Rainplay_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
